f85cd852e3cbd869de76d2903f2c787fb42df0ba
KingEusolius <julian_smithoz@yahoo.de>
2023-05-04 11:37:44 +0200

    -Update character.py - update main.py - added pathfinding.py
    Added pathfinding (breadth first search)
    Character can now move along chosen path

--- character.py
+++ character.py
@@ -2,27 +2,32 @@ import pygame
 
 
 class Character:
-    def __init__(self, anim_player):
+    def __init__(self, anim_player, clear_path):
         self.class_name = 'Cavalier'
         self.state = 'idle'
         self.direction = 'down'
         self.animation_index = 0
         self.anim_player = anim_player
 
-        self.direction = 1
+        self.direction_x = 0
+        self.direction_y = 0
+
+        self.waypoints = []
 
         self.img = anim_player.get_image(self.class_name, self.state, self.animation_index)
         self.img = pygame.transform.scale(self.img, (64, 64))
 
-        self.position_x = 100
-        self.position_y = 100
+        self.position_x = 2 * 64
+        self.position_y = 2 * 64
 
         self.target_position_x = 400
         self.target_position_y = 100
 
+        self.clear_path_for_game = clear_path
+
     def update(self):
         self.img = self.anim_player.get_image(self.class_name, self.state, int(self.animation_index))
-        if self.direction < 0:
+        if self.direction_x < 0:
             self.img = pygame.transform.flip(self.img, True, False)
         finished = self.play_animation()
 
@@ -30,9 +35,10 @@ class Character:
 
     def state_transition_handling(self, finished):
         if self.state == 'walk':
-            if self.position_x != self.target_position_x:
-                self.move(pygame.math.Vector2(self.direction, 0))
+            if len(self.waypoints) > 0:
+                self.move(pygame.math.Vector2(self.direction_x, self.direction_y))
             else:
+                self.clear_path_for_game()
                 self.change_state('idle')
 
         elif self.state == 'attack':
@@ -58,25 +64,36 @@ class Character:
         return False
 
     def move(self, amount):
-        self.position_x += amount.x
-        self.position_y += amount.y
+        self.position_x += amount.x * 2
+        self.position_y += amount.y * 2
+        if int(self.position_x) == int(self.waypoints[0][0]) and int(self.position_y) == int(self.waypoints[0][1]):
+            del self.waypoints[0]
+            if len(self.waypoints):
+                self.set_target_position(self.waypoints[0][0], self.waypoints[0][1])
 
-    def set_target_position(self, x):
-        self.target_position_x = x
-        self.get_direction(self.target_position_x)
+    def set_target_position(self, x, y):
+        self.get_direction(x, y)
 
     def change_state(self, state):
         self.animation_index = 0
         self.state = state
 
-    def get_direction(self, target):
-        if target == self.position_x:
-            return
+    def get_direction(self, target_x, target_y):
+        if target_x == self.position_x:
+            self.direction_x = 0
 
-        if target < self.position_x:
-            self.direction = -1
-        else:
-            self.direction = 1
+        if target_x < self.position_x:
+            self.direction_x = -1
+        if target_x > self.position_x:
+            self.direction_x = 1
+
+        if target_y == self.position_y:
+            self.direction_y = 0
+
+        if target_y < self.position_y:
+            self.direction_y = -1
+        if target_y > self.position_y:
+            self.direction_y = 1
 
     def draw(self, screen):
         screen.blit(self.img, (self.position_x, self.position_y))
