72079529463312def02ae354280b5d8882019b59
KingEusolius <julian_smithoz@yahoo.de>
2023-05-09 09:05:27 +0200

    -updated character.py
    added more attributes to character class
    outline will be drawn
    health is updated and drawn

--- character.py
+++ character.py
@@ -1,32 +1,51 @@
 import pygame
 
+pygame.font.init()
+my_font = pygame.font.SysFont('New Times Roman', 30)
 
 class Character:
-    def __init__(self, anim_player, clear_path):
-        self.class_name = 'Cavalier'
+    def __init__(self, anim_player, clear_path, pos, class_name):
+        self.class_name = class_name
         self.state = 'idle'
         self.direction = 'down'
         self.animation_index = 0
         self.anim_player = anim_player
+        self.health = 2
 
-        self.direction_x = 0
+        if class_name == 'cavalier':
+            self.direction_x = 0
+        else:
+            self.direction_x = -1
         self.direction_y = 0
 
         self.waypoints = []
 
         self.img = anim_player.get_image(self.class_name, self.state, self.animation_index)
-        self.img = pygame.transform.scale(self.img, (64, 64))
+        self.stats_img = pygame.Surface((64, 64 // 4))
+        self.color = (0, 0, 255)
+        self.stats_img.fill(self.color)
+        text_surface = my_font.render(f'{self.health} / {self.health}', True, (0, 0, 0))
+        text_width = text_surface.get_width()
+        text_pos = (64 - text_width) // 2
+        self.stats_img.blit(text_surface, (text_pos, 0))
 
-        self.position_x = 2 * 64
-        self.position_y = 2 * 64
+        self.outline = anim_player.get_outline(self.class_name, self.state, self.animation_index)
+
+        self.position_x = pos.x * 64
+        self.position_y = pos.y * 64
 
         self.target_position_x = 400
         self.target_position_y = 100
 
         self.clear_path_for_game = clear_path
 
+        self.selected = False
+
+
+
     def update(self):
         self.img = self.anim_player.get_image(self.class_name, self.state, int(self.animation_index))
+        self.outline = self.anim_player.get_outline(self.class_name, self.state, int(self.animation_index))
         if self.direction_x < 0:
             self.img = pygame.transform.flip(self.img, True, False)
         finished = self.play_animation()
@@ -39,6 +58,7 @@ class Character:
                 self.move(pygame.math.Vector2(self.direction_x, self.direction_y))
             else:
                 self.clear_path_for_game()
+                self.is_selected(False)
                 self.change_state('idle')
 
         elif self.state == 'attack':
@@ -47,7 +67,11 @@ class Character:
 
         elif self.state == 'take_hit':
             if finished:
+                self.health -= 1
+                self.update_status()
                 self.change_state('idle')
+                if self.health <= 0:
+                    self.change_state('death')
 
         elif self.state == 'death':
             if finished:
@@ -95,5 +119,27 @@ class Character:
         if target_y > self.position_y:
             self.direction_y = 1
 
+    def is_selected(self, value):
+        self.selected = value
+
+    def update_status(self):
+        #self.stats_img = pygame.Surface((self.width, self.height // 4))
+        self.stats_img.fill(self.color)
+        text_surface = my_font.render(f'{self.health} / {self.health}', True, (0, 0, 0))
+        text_width = text_surface.get_width()
+        text_pos = (64 - text_width) // 2
+        self.stats_img.blit(text_surface, (text_pos, 0))
+
     def draw(self, screen):
         screen.blit(self.img, (self.position_x, self.position_y))
+
+        if self.selected:
+            for point in self.outline:
+                if self.direction_x < 0:
+                    x = -point[0] + 64 + self.position_x
+                else:
+                    x = point[0] + self.position_x
+                y = point[1] + self.position_y
+                pygame.draw.line(screen, (255, 0, 0), (x, y), (x, y), 4)
+
+        screen.blit(self.stats_img, (self.position_x, self.position_y - 16))
