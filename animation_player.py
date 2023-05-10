a7d02ae89931cb337152068b323f6f41ffb0ebd3
KingEusolius <julian_smithoz@yahoo.de>
2023-05-10 11:08:26 +0200

    Update animation_player.py

--- animation_player.py
+++ animation_player.py
@@ -15,8 +15,8 @@ class Animation_Player:
             for state in state_list:
                 self.classes[f'{cl}'][f'{state}'] = []
                 self.classes_outlines[f'{cl}'][f'{state}'] = []
-                for pic in os.listdir(f'graphics/{cl}/{state}'):
-                    current_img = pygame.image.load(f'graphics/{cl}/{state}/{pic}').convert_alpha()
+                for pic in os.listdir(f'graphics/units/{cl}/{state}'):
+                    current_img = pygame.image.load(f'graphics/units/{cl}/{state}/{pic}').convert_alpha()
                     current_img = pygame.transform.scale(current_img, (64, 64))
                     self.classes[f'{cl}'][f'{state}'].append(current_img)
                     self.classes_outlines[f'{cl}'][f'{state}'].append(pygame.mask.from_surface(current_img))
@@ -30,6 +30,5 @@ class Animation_Player:
         return self.classes_outlines[class_name][class_state][index].outline()
 
     def is_animation_over(self, index):
-        if int(index) >= 4:
-            return True
-        return False
+        return int(index) >= 4
+
