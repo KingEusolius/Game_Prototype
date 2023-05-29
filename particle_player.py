b8b37e712ea78f53a6017dc3cc895255e4ea9b97
KingEusolius <julian_smithoz@yahoo.de>
2023-05-29 10:50:08 +0200

    Update particle_player.py
    -added first logic when particles are played

--- particle_player.py
+++ particle_player.py
@@ -18,13 +18,11 @@ class Particle_Player:
         return self.particles[particle_name][index]
 
     def is_animation_over(self, index):
-        if int(index) >= 4:
-            return True
-        return False
+        return int(index) >= 4
 
 
 class Particle:
-    def __init__(self, particle_player, particle_type, position, clear_particle):
+    def __init__(self, particle_player, particle_type, position, clear_particle, enemy_list, player_list):
         self.particle_player = particle_player
         self.animation_index = 0
         self.particle_type = particle_type
@@ -32,6 +30,19 @@ class Particle:
         self.position_x = position[0] * 64
         self.position_y = position[1] * 64
         self.clear_particle = clear_particle
+        if particle_type == 'inferno':
+            for enemy in enemy_list:
+                if self.position_x == enemy.position_x and self.position_y == enemy.position_y:
+                    enemy.change_state('take_hit')
+                    enemy.take_damage(2)
+
+        elif particle_type == 'update':
+            for player in player_list:
+                if self.position_x == player.position_x and self.position_y == player.position_y:
+                    player.change_state('take_hit')
+                    player.class_name = player.class_name.replace("minor", "major")
+                    player.get_stats(player.dictionary)
+                    print(player.class_name)
 
     def play_animation(self):
         self.animation_index += 0.2
